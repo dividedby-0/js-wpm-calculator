39fdadf8ad83b75577dafc6c3f7970f205bcf745
Alessandro <a.ghio86@gmail.com>
2022-08-05 19:01:52 +0300

    add readingtest page + mutation observer

--- common/router.js
+++ common/router.js
@@ -1,5 +1,7 @@
 const root = document.getElementById('root');
 
+// start routing logic
+
 let intro = '';
 let readingtest = '';
 let results = '';
@@ -31,10 +33,31 @@ main();
 const startTest = (pathname) => {
   root.classList.add('slide-out-top');
   window.history.pushState({}, pathname, window.location.origin + pathname);
-  setTimeout(() => (root.innerHTML = routes[pathname]), 2000);
-  //todo fix root position off screen
+  // setTimeout(() => (root.innerHTML = routes[pathname]), 2000);
+  setTimeout(() => root.classList.remove('slide-out-top'), 3000);
+  setTimeout(() => (root.innerHTML = readingtest), 2000);
+  setTimeout(() => root.classList.add('show-reading-test'), 1000);
 };
 
 window.onpopstate = () => {
   root.innerHTML = routes[window.location.pathname];
 };
+
+// end routing logic
+
+// start element class mutation observer
+
+let observer = new MutationObserver(function () {
+  if (root.classList == 'show-reading-test') {
+    console.log(root.classList.contains('show-reading-test'));
+  }
+});
+
+observer.observe(root, {
+  attributes: true,
+  attributeFilter: ['class'],
+  childList: false,
+  characterData: false,
+});
+
+// end element class mutation observer
